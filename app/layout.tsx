9f127a28416d41c41c7a9ae582986771c575a99b
swastik <swastikpatel29@gmail.com>
2024-02-07 14:37:15 +0530

    basic setup of themes and navbar done with shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(inter.className, "antialiased min-h-screen pt-12 bg-slate-50")}>{children}</body>
     </html>
   );
 }

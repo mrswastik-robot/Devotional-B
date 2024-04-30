08df70b42089a142195f85ed502c07eefbe3be6f
auraticabhi <abhijeetgupta989@gmail.com>
2024-05-01 00:20:35 +0530

    sidebar issue fixed

--- components/DesktopSidebar.tsx
+++ components/DesktopSidebar.tsx
@@ -45,7 +45,7 @@ export function Sidebar({ className, playlists }: SidebarProps) {
   }
 
   return (
-    <div className={cn("pb-1 border border-gray-300 rounded-lg sticky lg:top-[4.2rem] bg-[#ffffff]", className)}>
+    <div className={cn("pb-1 border border-gray-300 rounded-lg sticky lg:top-[4.2rem] max-h-[46rem] bg-[#ffffff]", className)}>
       <div className="space-y-4 py-4">
         <div className="px-1 py-2">
           <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">

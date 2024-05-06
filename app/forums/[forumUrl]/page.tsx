f18188ff18edb37436cb9a0c5258a7fe58e3880c
auraticabhi <abhijeetgupta989@gmail.com>
2024-05-06 20:50:55 +0530

    forum page updated

--- app/forums/[forumUrl]/page.tsx
+++ app/forums/[forumUrl]/page.tsx
@@ -131,9 +131,101 @@ const ForumsPage = ({ params: { forumUrl } }: Props) => {
       ,[forumUrl, router, joined])
     
     return (
-        <div>
-      
+      <div>
+      <div className="" />
+      <div className="w-full h-[9rem] relative border-white border-[3px]">
+              <div className="w-full h-full">
+                <Image
+                  fill
+                  src={
+                    forumDetails.bannerImageURL==null
+                      ? "https://static.vecteezy.com/system/resources/thumbnails/000/701/690/small/abstract-polygonal-banner-background.jpg"
+                      : forumDetails.bannerImageURL
+                  }
+                  alt="profile picture"
+                  referrerPolicy="no-referrer"
+                />
+              </div>
+            </div>
+      <div className="h-18 bg-white rounded-b-md">
+        <div className="mx-auto container px-12 py-2 flex relative flex-col">
+          {/* <div className="w-16 absolute h-16 bottom-6 rounded-full bg-green-400 border-white border-2" /> */}
+          <Avatar className="w-24 absolute h-24 bottom-6 rounded-full border-white border-[3px]">
+              <div className="">
+                <Image
+                  fill
+                  src={
+                    forumDetails.imageURL==null
+                      ? "https://www.adobe.com/content/dam/cc/us/en/creativecloud/design/discover/mascot-logo-design/mascot-logo-design_fb-img_1200x800.jpg"
+                      : forumDetails.imageURL
+                  }
+                  alt="profile picture"
+                  referrerPolicy="no-referrer"
+                />
+              </div>
+              <AvatarFallback>SP</AvatarFallback>
+            </Avatar>
+          <div className="flex items-center">
+            <h4 className="ml-[6.5rem] text-2xl font-bold text-gray-700">
+              {forumDetails.name}
+            </h4>
+            <button className="ml-4 text-sm text-green-400 font-semibold border border-green-400 py-1 px-3 rounded-md focus:outline-none" onClick={joinInForum}>
+              {joined ? "JOINED" : "JOIN"}
+            </button>
+          </div>
+          <p className="ml-[6.5rem] text-sm text-gray-600">r/{forumDetails.uniqueForumName}</p>
         </div>
+      </div>
+      <div className="">
+        <div className="flex container mx-auto py-2 px-4 items-start">
+          {/* Left Column (Posts) */}
+          <div className="w-2/3">
+            
+            <ForumsPostFeed newPost={false} forumURL={forumUrl}/>
+            {/* {forumPost?.map((post:any, index:any) => (
+              <SubRedditPost key={index}/>
+            ))} */}
+          </div>
+
+          {/* >Right Column (sidebar) */}
+          <div className="w-1/3 ml-4 bg-white rounded-md mt-1">
+            <div className="bg-green-400 py-4 px-2 rounded-t-md">
+              <p className="text-sm text-gray-900 font-bold">About Community</p>
+            </div>
+            <div className="p-2">
+              <p>{forumDetails.description?parse(forumDetails.description):""}</p>
+              <div className="flex w-full mt-2 font-semibold">
+                <div className="w-full">
+                  <p>{forumDetails.noOfMembers}</p>
+                  <p className="text-sm">Members</p>
+                </div>
+                <div className="w-full">
+                  <p>{forumDetails.numOfPosts}</p>
+                  <p className="text-sm">total Posts</p>
+                </div>
+              </div>
+              <div className="w-full h-px bg-gray-300 my-4" />
+              <p className="text-md mb-4 flex">
+                <b>Created On : {` `}</b>{" "}
+                <span>{dateString && <p className='ml-1 mt-[2.5px] font-semibold text-gray-900 text-sm'>{dateString}</p>}</span>
+              </p>
+              <Link href={`/forums/createForum`}>
+              <button className="focus:outline-none rounded-md w-full py-3 text-gray-900 font-semibold bg-green-400">
+                CREATE POST
+              </button>
+              </Link>
+              <p className="text-md mb-1 mt-3">
+                <b className='mb-1'>Rules 👇</b>
+
+                <div>{forumDetails.rules}</div>
+              </p>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+        </div>
+
     );
 }
 

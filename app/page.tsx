b775194625fe1ebaa6ffef84895d09a7236fbed2
auraticabhi <abhijeetgupta989@gmail.com>
2024-07-01 19:58:41 +0530

    error fixed

--- app/page.tsx
+++ app/page.tsx
@@ -585,7 +585,7 @@ export default function Home() {
         <div className="text-3xl font-semibold">Discover Devotional Content</div>
         <div className="text-lg font-semibold mt-[19px]">Your Pathway to conect to God</div>
         <div className="text-base mt-[10px]">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
         </div>
       </div>
       </div>
